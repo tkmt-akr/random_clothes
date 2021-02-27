d30abad7be49f592eeefd095f8dd97382fc90de7
akira.tsukamoto <sooccer.akira6@uqmobile.jp>
2021-02-27 14:38:44 +0900

    Add NewServlet.java

---
+++ src/models/Cloth.java
@@ -0,0 +1,61 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllClothes",
+        query = "SELECT m FROM Cloth AS m ORDER BY m.id DESC"
+    ),
+    @NamedQuery(
+            name = "getClothesCount",
+            query = "SELECT COUNT(m) FROM Cloth AS m"
+    )
+})
+@Table(name = "clothes")
+public class Cloth {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
