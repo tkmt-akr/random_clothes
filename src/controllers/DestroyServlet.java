78343a21ac621bce45ae6a680609b879f4541f65
akira.tsukamoto <sooccer.akira6@uqmobile.jp>
2021-02-27 19:54:40 +0900

    Modify DestroyServlet.java

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Cloth;
 import utils.DBUtil;
 
 /**
@@ -34,18 +34,20 @@ public class DestroyServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            /*
+            セッションスコープからIDを取得して
+            該当のID1件のみをデータベースから取得
+            */
+            Cloth c = em.find(Cloth.class, (Integer)(request.getSession().getAttribute("cloth_id")));
 
             em.getTransaction().begin();
-            em.remove(m);       // データ削除
+            em.remove(c);       // データ削除
             em.getTransaction().commit();
             request.getSession().setAttribute("flush", "削除が完了しました。");
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
-            request.getSession().removeAttribute("message_id");
+            request.getSession().removeAttribute("cloth_id");
 
             // indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
