d6f7a3baa5e12ceacf3e18f1967a295d9080ee9f
akira.tsukamoto <sooccer.akira6@uqmobile.jp>
2021-02-27 19:45:42 +0900

    Modify UpdateServlet

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
-import models.validators.MessageValidator;
+import models.Cloth;
+import models.validators.ClothValidator;
 import utils.DBUtil;
 
 /**
@@ -38,31 +38,27 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            /*
+            セッションスコープからIDを取得して
+            該当のIDの1件のみをデータベースから取得
+            */
+            Cloth c = em.find(Cloth.class, (Integer)(request.getSession().getAttribute("cloth_id")));
 
             // フォームの内容を各フィールドに上書き
-            String title = request.getParameter("title");
-            m.setTitle(title);
-
-            String content = request.getParameter("content");
-            m.setContent(content);
-
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+            c.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
          // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
-            List<String> errors = MessageValidator.validate(m);
+            List<String> errors = ClothValidator.validate(c);
             if(errors.size() > 0) {
                 em.close();
 
                 // フォームに初期値を設定、さらにエラーメッセージを送る
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("message", m);
+                request.setAttribute("cloth", c);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/display/edit.jsp");
                 rd.forward(request, response);
             } else {
                 // データベースを更新
@@ -72,7 +68,7 @@ public class UpdateServlet extends HttpServlet {
                 em.close();
 
                 // セッションスコープ上の不要になったデータを削除
-                request.getSession().removeAttribute("message_id");
+                request.getSession().removeAttribute("cloth_id");
 
                 // indexページへリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
